1f5c71a8c4afa50d2f9250d3476b19259be9f4f2
huangsifang <17816869682@163.com>
2017-07-10 20:52:05 +0800

    客户端发送消息给所有用户，用户退出时删除socket

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 1.
 'telnet'不是内部或外部命令解决方法
 控制面板-程序-打开或关闭Windows功能-勾选Telnet服务器和Telnet客户端
+
+2.
+clientList.splice(clientList.indexOf(cleanup[i]), 1)
+IE9以前的版本都不支持此方法
+if(!Array.indexOf){
+  	Array.prototype.indexOf = function(el){
+	 	for (var i=0,n=this.length; i<n; i++){
+	 		if (this[i] === el){
+	  			return i;
+	 		}
+	 	}
+	 	return -1;
+   	} 
+}
--- chat.js
+++ chat.js
@@ -1,12 +1,52 @@
 var net = require('net');
 
-var chatServer = net.createServer();
+var chatServer = net.createServer(),
+	clientList = [];
 
 chatServer.on('connection', function(client) {
-	client.write('Hi!\n');
-	client.write('Bye!\n');
+	client.name = client.remoteAddress + ':' + client.remotePort;
+	client.write(client.name + 'joined!\n');
 
-	client.end();
+	clientList.push(client);
+
+	client.on('data', function(data) {
+		broadcast(data, client);
+	})
+
+	client.on('end', function() {
+		console.log(client.name + ' quit');
+		for (var i=0,n=clientList.length; i<n; i++) {
+		 	if (clientList[i] === client) {
+			  	clientList.splice(i, 1);
+			}
+		}
+	})
+
+	client.on('error', function(e) {
+		console.log(e);
+	})
 })
+
+function broadcast(message, client) {
+	var cleanup = [];
+	for(var i=0; i<clientList.length; i+=1) {
+		if(client !== clientList[i]) {
+			if(clientList[i].writable) {
+				//把数据发送给除发送者外的其他客户端
+				clientList[i].write(client.name + " says: " + message + '\n');
+			} else {
+				cleanup.push(clientList[i]);
+				clientList[i].destroy();
+			}
+		}
+	}
+	for(i=0; i<cleanup.length; i+=1) {
+		for (var i=0,n=clientList.length; i<n; i++) {
+		 	if (clientList[i] === cleanup[i]) {
+			  	clientList.splice(i, 1);
+			}
+		}
+	}
+}
 
 chatServer.listen(9000);
